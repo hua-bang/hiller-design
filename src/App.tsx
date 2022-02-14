213ec6ac2ed51da03f13536644f987dd96cfd706
hua-bang <47221942+hua-bang@users.noreply.github.com>
2022-02-14 12:05:27 +0800

    feat: alert component demo

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
 import React, { useState } from 'react';
+import { Alert } from './components/index';
 import './App.css';
 
 function App() {
@@ -6,6 +7,7 @@ function App() {
 
   return (
     <div className="App">
+      <Alert />
       <header className="App-header">
         <p>{ count }</p>
         <button
---
+++ src/components/alert/alert.tsx
@@ -0,0 +1,36 @@
+import React from 'react';
+import { AlertProps, KindMap, Kind } from './interface';
+import './style/index.css';
+
+const prefixCls = 'hiller-alert';
+const defaultKind: Kind = 'info';
+const defaultText = 'alert';
+
+const kindsColorMap: KindMap = {
+  info: '#5352ED',
+  positive: '#2ED573',
+  negative: '#FF4757',
+  warning: '#FFA502'
+};
+
+const Alert: React.FC<AlertProps> = props => {
+  const {
+    kind = defaultKind,
+    children = defaultText,
+    ...rest
+  } = props;
+  
+  return (
+    <div
+      className={prefixCls}
+      style={{
+        background: kindsColorMap[kind as Kind]
+      }}
+      {...rest}
+    >
+      {children}
+    </div>
+  );
+};
+
+export default Alert;
